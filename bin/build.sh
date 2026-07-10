2849df93b82692071dab491b280133fe306eec85
Andrew Marcuse <fileformat@gmail.com>
2026-07-10 17:10:48 -0400

    Switch from Cloudflare Pages to Workers

--- bin/build.sh
+++ bin/build.sh
@@ -33,4 +33,21 @@ npm run build
 echo "INFO: merging index files"
 tar cvzf dist/util/sourceData.tgz dist/api/search-*.json
 
+echo "INFO: CI=${CI:-not set}"
+echo "INFO: CF_PAGES=${CF_PAGES:-not set}"
+echo "INFO: WORKERS_CI=${WORKERS_CI:-not set}"
+echo "INFO: WORKERS_CI_COMMIT_SHA=${WORKERS_CI_COMMIT_SHA:-not set}"
+
+if [ "${CI:-0}" == "true" ]; then
+    echo "INFO: running Cloudflare Pages post-build script"
+    HTML_PAGES=$(find dist -type f -name "*.html" | grep -v "index.html$" | grep -v "404.html$")
+    for PAGE in $HTML_PAGES; do
+        echo "INFO: renaming $PAGE"
+        mv "${PAGE}" "${PAGE}.html"
+    done
+else
+    echo "INFO: skipping Cloudflare Pages post-build script"
+fi
+
+
 echo "INFO: build completed at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
